ee3bc7dd877b0aefae36d242f20b8c5d9ea5cb43
lawrencebii <80627941+lawrencebii@users.noreply.github.com>
2023-01-29 12:26:56 +0300

    Channel Call

---
+++ lib/utils/settings.dart
@@ -0,0 +1,3 @@
+const appId = '6ae6c506f3a84e78ae248f3cf0a46bdc';
+const token =
+    '007eJxTYKjZmFo1VWPn67alOjcNp+pe7GFX/9V4a/39ODmbn/frt6xVYDBLTDVLNjUwSzNOtDBJNbdITDUysUgzTk4zSDQxS0pJfqFzLbkhkJEhjf8jMyMDBIL4HAxJmZnxJanFJQwMAIdyI7w=';
